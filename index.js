503d984dd955fe178401bd3c777d808958a8518e
Alf Eaton <eaton.alf@gmail.com>
2018-03-21 15:39:37 +0000

    Export all components

--- index.js
+++ index.js
@@ -1,3 +1,5 @@
 var SplitPane = require('./lib/SplitPane');
+var Pane = require('./lib/Pane');
+var Resizer = require('./lib/Resizer');
 
-module.exports = SplitPane;
+module.exports = { SplitPane, Pane, Resizer };
